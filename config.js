e5eb1e71a97b988a84411e114787d66964c00c96
Smith <andras@sexybuddha.hu>
2017-08-02 19:31:10 +0200

    working amember token authentication api

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const minimist = require('minimist');
 
 let argv = minimist(process.argv.slice(2), {
     default:               {
-        API_URL:    'https://localhost/base/xchat-api',
+        API_URL:    'https://x-token.com/base/xchat-api',
         API_KEY:    'yyy',
         PORT:       8090
     }
--- server.js
+++ server.js
@@ -7,22 +7,9 @@ const express = require("express");         // web framework external module
 const serveStatic = require('serve-static');// serve static files
 const socketIo = require("socket.io");      // web socket external module
 const easyrtc = require("easyrtc");         // EasyRTC external module
+const got = require("got");
 
-easyrtc.setOption("logLevel", "info");// (debug|info|warning|error|none)
-easyrtc.setOption("logDateEnable", true);
-easyrtc.setOption("appAutoCreateEnable", false);
-easyrtc.setOption("roomAutoCreateEnable", true);//TODO: create room with model as owner
-easyrtc.setOption("roomDefaultEnable", false);
-//easyrtc.setOption("connectionDefaultField", {"foo":{fieldValue:"bar",fieldOption:{isShared:true}}});
-easyrtc.setOption("apiEnable", false);
-easyrtc.setOption("apiLabsEnable", false);
-easyrtc.setOption("demosEnable", false);
-easyrtc.setOption("updateCheckEnable", false);
-easyrtc.setOption("usernameRegExp", /^[a-z0-9_.-]{1,32}$/i);
-easyrtc.setOption("presenceShowRegExp", /^(online|offline)$/);
-easyrtc.setOption("presenceStatusRegExp", /^(.){0,255}$/);
-
-process.title = "xchat-server";
+process.title = 'xchat-server';
 
 var app = express();
 app.use(serveStatic('static', {'index': ['index.html']}));
@@ -34,32 +21,18 @@ var webServer = https.createServer({
 
 var socketServer = socketIo.listen(webServer, {"log level":1});
 
-
-//DEBUG
-// Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
-        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
-            callback(err, connectionObj);
-            return;
-        }
-
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":true});
-
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
-
-        callback(err, connectionObj);
-    });
-});
-
-// To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
-    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
-});
-//DEBUG
-
-
+easyrtc.setOption("logLevel", "info");// (debug|info|warning|error|none)
+easyrtc.setOption("logDateEnable", true);
+easyrtc.setOption("appAutoCreateEnable", false);
+easyrtc.setOption("roomAutoCreateEnable", true);//TODO: create room with model as owner
+easyrtc.setOption("roomDefaultEnable", false);
+easyrtc.setOption("apiEnable", false);
+easyrtc.setOption("apiLabsEnable", false);
+easyrtc.setOption("demosEnable", false);
+easyrtc.setOption("updateCheckEnable", false);
+easyrtc.setOption("usernameRegExp", /^[a-z0-9_.-]{1,32}$/i);
+easyrtc.setOption("presenceShowRegExp", /^(online|offline)$/);
+easyrtc.setOption("presenceStatusRegExp", /^(.){0,255}$/);
 
 var rtcServer = easyrtc.listen(
     app,
@@ -84,38 +57,57 @@ var rtcApp = function(err, appObj) {
     appObj.events.on('authenticate', authenticateListener);
 
     appObj.events.on('authenticated', authSuccess);
+
+    appObj.events.on('easyrtcAuth', rtcAuthCallback);
+
+    appObj.events.on('easyrtcCmd', rtcCmd);
 };
 
 function authenticateListener(socket, easyrtcid, appName, username, credential, easyrtcAuthMessage, next) {
-    console.log('! authenticate', easyrtcid, appName, username, credential);//, easyrtcAuthMessage);
+    console.log('! authenticate', username);// easyrtcid, appName, username, credential);
 
-    let validTokenData = validateAuthToken(username, credential.token);
-
-    if (validTokenData) {
-        credential.balance = validTokenData.balance || 0;
-        credential.avatar_url = validTokenData.avatar_url || '';
-
-        customersData[easyrtcid] = credential;
-        next(null);
-    }
+    if (username !== credential.name) next('Invalid auth.');
     else {
-        //TODO: next action? reload? ajax for new token?
-        next(new easyrtc.util.ConnectionError("Failed auth."));
+        validateAuthToken(credential)
+            .then(validTokenData => {
+                credential.balance = validTokenData.balance || 0;
+
+                customersData[easyrtcid] = validTokenData;
+                next(null);
+            })
+            .catch(err => {
+                //TODO: next action? reload? ajax for new token?
+                next('Failed auth.');
+            });
     }
 }
 
-function validateAuthToken(name, token) {
-    let re = false;
+function validateAuthToken(cred) {
+    return new Promise((resolve, reject) => {
+        apiCall({token: cred.token})
+            .then(res => {
+                if (res.body.ok === true) resolve(res.body);
+                else reject(res.body);
+            })
+            .catch(err => {
+                console.error('validateAuthToken.error', err);
+                reject(err);
+            });
+    });
+}
 
-    //TODO: got that json with ajax headers & api key posted
-    console.log(CONFIG.API_KEY, CONFIG.API_URL);
-    //if ok, return all necessary and available data
+function apiCall(data) {
+    data.api_key = CONFIG.API_KEY;
 
-    return re;
+    return got.post(CONFIG.API_URL, {
+        body: data,
+        json: true,
+        form: true
+    });
 }
 
 function authSuccess(connectionObj, next) {
-    console.log('! authenticated');
+    console.log('! authenticated', connectionObj.getUsername());
 
     let easyrtcid = connectionObj.getEasyrtcid();
 
@@ -138,7 +130,32 @@ function authSuccess(connectionObj, next) {
     }
 }
 
-//TODO: block users from setting presence or atleast filter status text
+function rtcAuthCallback(socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
+        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+            callback(err, connectionObj);
+            return;
+        }
+
+        connectionObj.setField('credential', msg.msgData.credential, {"isShared": false});
+        console.log('! cred', connectionObj.getFieldValueSync('credential'));
+
+        callback(err, connectionObj);
+    });
+}
+
+function rtcCmd(connectionObj, msg, socketCallback, next) {
+    switch(msg.msgType) {
+        case 'setPresence':
+        case 'roomJoin':
+        case 'getRoomList':
+            console.log('CMD BLOCKED', msg.msgType);
+            next(null);
+            break;
+        default:
+            easyrtc.events.defaultListeners.easyrtcCmd(connectionObj, msg, socketCallback, next);
+    }
+}
 
 //listen on port PORT
 webServer.listen(CONFIG.PORT, function () {
